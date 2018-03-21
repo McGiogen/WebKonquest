807274a29244c4e85385bee605c151f05a8574ae
Gioele Masini <gioele.masini@maggioli.it>
2018-03-21 22:12:05 +0100

    Fix core logger. Dependencies update.

--- src/game/logger.ts
+++ src/game/logger.ts
@@ -1,4 +1,4 @@
-import structuredLog from 'structured-log';
+import * as structuredLog from 'structured-log';
 
 export const log = structuredLog.configure()
     .writeTo(new structuredLog.ConsoleSink())
